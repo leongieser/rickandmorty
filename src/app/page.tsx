02b0a11cad76ced5864b0e34e4aa64ba62d5fe11
Leon Gieser <leon.gieser@gmail.com>
2023-10-15 14:13:25 +0200

    :lipstick: custom font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,22 @@
+import localFont from 'next/font/local'
+
+const ramFont = localFont({
+	src: '../lib/fonts/WubbaLubbaDubDub.woff2',
+	// src: '../lib/fonts/getSchwifty.woff2',
+	preload: true,
+	display: 'swap',
+	weight: 'normal',
+	variable: '--font-ram',
+})
+
 export default function Home() {
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-center p-24">
-			Wubba Lubba dub-dub
+			<h1
+				className={`${ramFont.className} thicc-outline text-8xl text-headingFill `}
+			>
+				Wubba Lubba dub-dub
+			</h1>
 		</main>
 	)
 }
