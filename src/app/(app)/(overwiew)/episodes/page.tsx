a42489c1ec7bb4b7ba9085f7e03e9b60e8abb01c
Leon Gieser <leon.gieser@gmail.com>
2023-10-16 09:59:44 +0200

    :poop: => :art: episodes page

--- src/app/(app)/(overwiew)/episodes/page.tsx
+++ src/app/(app)/(overwiew)/episodes/page.tsx
@@ -1,39 +1,13 @@
 import type { Metadata } from 'next'
-import localFont from 'next/font/local'
 import Link from 'next/link'
+import { EpisodeCard, SectionHeader } from '@/ram/components'
 import { getAllEpisodes } from '@/ram/lib/api'
 import { cn } from '@/ram/lib/utils'
-import { AllEpisodesResponse, Episode } from '@/ram/types'
-
-export async function generateStaticParams() {
-	const episodes: AllEpisodesResponse = await getAllEpisodes()
-
-	const nOfEpisodes = Number(episodes.info.count)
-	const staticParams = []
-
-	for (let i = 1; i < nOfEpisodes; i++) {
-		staticParams.push({
-			params: {
-				id: `${i}`,
-			},
-		})
-	}
-
-	return staticParams
-}
-
-const ramFont = localFont({
-	src: './../../../../lib/fonts/WubbaLubbaDubDub.woff2',
-	preload: true,
-	display: 'swap',
-	weight: 'normal',
-	variable: '--font-ram',
-})
 
 export const metadata: Metadata = {
 	title: 'Episodes',
 	description:
-		'An overview of all the episodes from the Rick and Morty series.',
+		'An overview of all the episodes from the Rick and Morty universe.',
 }
 
 export default async function Episodes({
@@ -44,19 +18,15 @@ export default async function Episodes({
 	const page =
 		typeof searchParams.page === 'string' ? Number(searchParams.page) : 1
 
-	const episodes: AllEpisodesResponse = await getAllEpisodes(page)
+	const episodes = await getAllEpisodes(page)
 
 	return (
-		<div className="flex w-full flex-grow flex-col items-center gap-8 p-5">
-			<h1
-				className={`${ramFont.className} thicc-outline mx-auto text-center text-7xl text-ram-primary `}
-			>
-				Episodes
-			</h1>
+		<section className="flex w-full flex-grow flex-col items-center gap-8 p-5">
+			<SectionHeader heading="Episodes" />
 			<div className="flex gap-4">
 				<Link
 					className={cn(
-						'w-20 rounded-md bg-ram-primary px-4 py-2 text-center  text-xl font-bold text-ram-secondary ring-4  ring-ram-secondary',
+						'w-20 select-none rounded-md bg-ram-primary px-4 py-2 text-center  text-xl font-bold text-ram-secondary ring-4  ring-ram-secondary',
 						{
 							'pointer-events-none bg-gray-600': page === 1,
 						}
@@ -66,12 +36,12 @@ export default async function Episodes({
 				>
 					prev
 				</Link>
-				<span className="w-20 rounded-md bg-ram-primary px-4 py-2 text-center  text-xl font-bold text-ram-secondary ring-4  ring-ram-secondary">
-					{page}
+				<span className="w-32 select-none rounded-md bg-ram-primary px-4 py-2 text-center  text-xl font-bold text-ram-secondary ring-4  ring-ram-secondary">
+					{page} of {episodes.info.pages || 3}
 				</span>
 				<Link
 					className={cn(
-						'w-20 rounded-md bg-ram-primary px-4 py-2 text-center  text-xl font-bold text-ram-secondary ring-4  ring-ram-secondary',
+						'w-20 select-none rounded-md bg-ram-primary px-4 py-2 text-center  text-xl font-bold text-ram-secondary ring-4  ring-ram-secondary',
 						{
 							'pointer-events-none bg-gray-600':
 								page === Number(episodes.info.pages),
@@ -84,25 +54,10 @@ export default async function Episodes({
 				</Link>
 			</div>
 			<section className="grid grid-cols-1 gap-5 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-				{episodes.results.map((episode: Episode) => (
-					<Link
-						href={`/episode/${episode.id}`}
-						className="flex h-full w-full max-w-sm cursor-pointer flex-col justify-between rounded-md bg-zinc-400 p-3 text-zinc-950"
-						key={episode.id}
-					>
-						<div className="">
-							<h1 className="text-2xl font-bold">
-								{episode.name}
-							</h1>
-							<p className="font-semibold">{episode.episode}</p>
-							<p className="font-semibold">
-								Aired on: {episode.air_date}
-							</p>
-						</div>
-						<div className="mt-auto w-full bg-red-200">asd</div>
-					</Link>
+				{episodes.results.map((ep) => (
+					<EpisodeCard key={`${ep.id}${ep.name}`} episode={ep} />
 				))}
 			</section>
-		</div>
+		</section>
 	)
 }
