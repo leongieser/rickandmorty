506bdcdd02bc914d73f0aa3286db71e18f8086be
Leon Gieser <leon.gieser@gmail.com>
2023-10-16 09:54:50 +0200

    :zap: move local/font into utils

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -1,6 +1,15 @@
+import localFont from 'next/font/local'
 import { ClassValue, clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs))
 }
+
+export const ramFont = localFont({
+	src: '../fonts/WubbaLubbaDubDub.woff2',
+	preload: true,
+	display: 'swap',
+	weight: 'normal',
+	variable: '--font-ram',
+})
