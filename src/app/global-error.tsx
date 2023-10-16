d3205b614243f8dc2df049a4b25c78235987c983
Leon Gieser <leon.gieser@gmail.com>
2023-10-16 14:36:04 +0200

    :goal_net: global-error Image

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { useEffect } from 'react'
+import Image from 'next/image'
 import { SectionHeader } from '@/ram/components'
 
 export default function GlobalError({
@@ -14,11 +15,24 @@ export default function GlobalError({
 		console.error(error)
 	}, [error])
 
+	/*
+	 *
+	 * Not stable yet
+	 *@see https://github.com/vercel/next.js/issues/55462
+	 */
 	return (
 		<html>
 			<body>
-				<SectionHeader heading="Pluto Was A Planet" />
-				<button onClick={() => reset()}>Try again</button>
+				<SectionHeader heading="Pluto Was A Planet..." />
+				<Image
+					src="/assets/jerry.png"
+					width={500}
+					height={500}
+					alt="Jerry looking cluless as usual"
+				/>
+				<button onClick={() => reset()}>
+					<SectionHeader heading="Try again" />
+				</button>
 			</body>
 		</html>
 	)
